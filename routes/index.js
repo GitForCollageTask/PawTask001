a7b8ce5918da1cd5f7194ba066467619fb7c1405
Ibnu Aziz Nurrohman <ibnuaziznu@gmail.com>
2017-12-03 11:55:20 +0700

    tambah bebarapa package yang dibutuhkan

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
-var router = express.Router();
+//var router = express.Router();
 var mysql = require('mysql');
-var json = require('res-json');
+var app = express();
+//var json = require('res-json');
+var session = require('express-session');
 
-    var con = mysql.createPool({
-    connectionLimirt : 50,
+    connection = mysql.createPool({
     host:'localhost',
     user:'root',
     password:'',
@@ -18,31 +19,67 @@ exports.home = function(req, res){
         title: "Sistem Informasi Mesjid Cibiru"
     });
 };
-exports.signup = function(req, res, next){
+
+exports.add = function(req, res){
+  res.render('add_customer',{page_title:"Add Customers - Node.js"});
+};
+
+exports.signup = function(req, res){
     res.render('sign-up', { title: 'Sign Up | SIMAC' });
-}; 
+
+};
+
+exports.save = function(req, res){
+     var input = JSON.parse(JSON.stringify(req.body));
+    connection.getConnection(function (error, connection) {
+        if(this.error){
+            this.error.release();
+            console.log(error);
+        }else{
+            var data = {
+            
+            id_masjid    : input.id_masjid,
+            nama_lengkap : input.nama_lengkap,
+            email        : input.email,
+            username     : input.username,
+            password     : input.password,
+            no_telpon    : input.no_telpon 
+        };
+            console.log("Connected!!");
+         var query = connection.query("INSERT INTO users set ? ",data, function(err, rows){
+            connection.release();
+            if (err)
+              console.log(err);
+         
+             res.redirect('/sign-up/add');
+        });
+      };
+   });
+};
+        
 exports.admin = function(req, res, next){
     res.render('admin', { title: 'Login Admin | SIMAC' });
 };
 
-exports.homeAdmin = function(req, res, next){
-    res.render('homeAdmin', { title: 'Home Admin | SIMAC' });
-        con.getConnection(function(error, tempCont){
-            if(!!error){
-                tempCont.release();
-                console.log('Error')
-            }else{
-                tempCont.query("SELECT * FROM users", function(error, rows, fields){
-                    tempCont.release();
+exports.homeAdmin = function(req, res){
+    
+        connection.getConnection(function(error, connection){
+                if(!!error){
+                    connection.release();
+                    console.log(error);
+                }else{
+                    console.log("Connected!!");
+                    connection.query("SELECT * FROM users", function(error, rows, fields){
+                    connection.release();
                     if(!!error){
-                        console.log('error');
-                        return;
+                        console.log(error);
                     }else{
-                        console.log(rows);
+                         res.render('homeAdmin', { title: 'Home Admin | SIMAC', data:rows });
                     }
                 });
-            } 
-        });
+                    
+            }; 
+        }); 
     };
     
 exports.session = function(req, res){
@@ -51,9 +88,9 @@ exports.session = function(req, res){
     if(req.body.username == 'ibnuaziz' && req.body.password == 'admin'){
         session.uniqueID = req.body.username;
     }
-    res.redirect('/homeAdmin');
+    res.redirect('/redirects');
 };
-exports.redirect = function(req, res){
+exports.redirects = function(req, res){
     session = req.session;
     if(session.uniqueID){
         res.redirect('/homeAdmin');
